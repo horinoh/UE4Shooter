96b593428c068154d00ef3b1445d7caf19156497
horinoh <development@horino.sakura.ne.jp>
2016-11-09 01:52:02 +0900

    追加

---
+++ Source/UE4Shooter/Private/Weapon/ShooterWeapon.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Shooter.h"
+#include "ShooterWeapon.h"
+
+AShooterWeapon::AShooterWeapon(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	//PrimaryActorTick.bCanEverTick = true;
+}
+
---
+++ Source/UE4Shooter/Public/Player/ShooterPlayerCameraManager.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Camera/PlayerCameraManager.h"
+#include "ShooterPlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AShooterPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
